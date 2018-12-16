c24c1da2403728262dc3553f3706baef096babdf
ddwbb <bogomolovma98@mail.ru>
2018-12-16 17:50:29 +0300

    Complite Lab
    Добавлены функции открытия/блокирования семафоров. Настроена совместная работа процессов

--- lab_4_semaphores/connector.c
+++ lab_4_semaphores/connector.c
@@ -5,19 +5,25 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 #include <sys/types.h>
 
+int sem_id;
 char * buffer;
 
-void init();
+void open_semaphore();
+void lock_semaphore();
+void unlock_semaphore();
+void init_shared_memory();
 void read_data();
 
 int main(int argc, char * argv[]) {
-    init();
+    init_shared_memory();
+    open_semaphore();
     read_data();
 }
 
-void init() {
+void init_shared_memory() {
     key_t key = 3972;
     int shm_id;
 
@@ -32,9 +38,41 @@ void init() {
     }
 }
 
+void open_semaphore() {
+    key_t key = 3972;
+
+    if ((sem_id = semget(key, 1, 0666)) == -1) {
+        perror("Semaphore opening");
+        exit(0);
+    }
+}
+
+void lock_semaphore() {
+    struct sembuf lock = { 0, -1, SEM_UNDO };
+
+    if (semop(sem_id, &lock, 1) == -1) {
+        perror("Lock failed");
+        exit(0);
+    }
+    printf("Semaphore locked\n");
+}
+
+void unlock_semaphore() {
+    struct sembuf unlock = { 0, 1, SEM_UNDO };
+
+    if (semop(sem_id, &unlock, 1) == -1) {
+        perror("Unlock failed");
+        exit(0);
+    }
+    printf("Semaphore unlocked\n");
+}
+
 void read_data() {
     while(1) {
+        lock_semaphore();
         printf("%s\n", buffer);
+        unlock_semaphore();
+        sleep(1);
     }
 }
 
--- lab_4_semaphores/spammer.c
+++ lab_4_semaphores/spammer.c
@@ -1,23 +1,68 @@
 #include <time.h>
-#include <error.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 #include <sys/types.h>
 
+int sem_id;
 char * buffer;
 
-void init();
+void lock_semaphore();
+void unlock_semaphore();
+void open_semaphore();
+void create_semaphore();
+void init_shared_memory();
 void spam();
 
 int main(int argc, char * argv[]) {
-    init();
-    spam();
+    pid_t pid;
+    init_shared_memory();
+    create_semaphore();
+    if (argc == 2) {
+        if ((pid = fork()) == 0) {
+            usleep(500000);
+            execv("./connector", argv);
+        } else {
+            spam();
+            kill(pid, SIGTERM);
+            unlock_semaphore();
+        }
+    } else {
+        spam();
+    }
 }
 
-void init() {
+void create_semaphore() {
+    key_t key = 3972;
+
+    if ((sem_id = semget(key, 1, IPC_CREAT | IPC_EXCL | 0666)) == -1) {
+        if (errno == EEXIST) {
+            open_semaphore();
+        } else {
+            perror("Semaphore creating");
+            exit(0);
+        }
+    } else {
+        printf("Semaphore created\n");
+    }
+    struct sembuf init = {0, 1, SEM_UNDO};
+    semop(sem_id, &init, 1);
+}
+
+void open_semaphore() {
+    key_t key = 3972;
+
+    if ((sem_id = semget(key, 1, 0666)) == -1) {
+        perror("Semaphore opening");
+        exit(0);
+    }
+}
+
+void init_shared_memory() {
     key_t key = 3972;
     int shm_id;
 
@@ -32,12 +77,34 @@ void init() {
     }
 }
 
+void lock_semaphore() {
+    struct sembuf lock = { 0, -1, SEM_UNDO };
+
+    if (semop(sem_id, &lock, 1) == -1) {
+        perror("Lock failed");
+        exit(0);
+    }
+    printf("Semaphore locked\n");
+}
+
+void unlock_semaphore() {
+    struct sembuf unlock = { 0, 1, SEM_UNDO };
+
+    if (semop(sem_id, &unlock, 1) == -1) {
+        perror("Unlock failed");
+        exit(0);
+    }
+    printf("Semaphore unlocked\n");
+}
+
 void spam() {
     struct tm timeval;
-    while(1) {
+    for(int i = 0; i < 10; i++) {
+        lock_semaphore();
         time_t timer = time(0);
         timeval = *localtime(&timer);
         sprintf(buffer, "%.2d:%.2d:%.2d", timeval.tm_hour, timeval.tm_min, timeval.tm_sec);
+        unlock_semaphore();
         sleep(1);
     }
 }
